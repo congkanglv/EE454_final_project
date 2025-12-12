45bba3f00c1e024113c9881d29f1a344acfa30b3
cesarpin034 <cesarpin@usc.edu>
2025-12-11 19:19:55 -0800

    Delete phase2pt2 directory

--- phase2pt2/task1/FPGA1/nios/nios_inst.v
+++
@@ -1,7 +0,0 @@
-	nios u0 (
-		.clk_clk                             (<connected-to-clk_clk>),                             //                          clk.clk
-		.reset_reset_n                       (<connected-to-reset_reset_n>),                       //                        reset.reset_n
-		.gpio_out_external_connection_export (<connected-to-gpio_out_external_connection_export>), // gpio_out_external_connection.export
-		.switch_external_connection_export   (<connected-to-switch_external_connection_export>)    //   switch_external_connection.export
-	);
-
